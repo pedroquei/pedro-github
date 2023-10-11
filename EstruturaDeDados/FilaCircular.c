17b5ec2cdd5cf30fb36b2bbf1af6480a0be54d88
Pedro Queiroz <tec567837@gmail.com>
2023-10-11 17:57:12 -0300

    ATV

--- EstruturaDeDados/FilaCircular.c
+++ EstruturaDeDados/FilaCircular.c
@@ -2,78 +2,70 @@
 #include <stdlib.h>
 
 #define TAM 5
+typedef struct SFila{
+    int fila[TAM];
+    int inicio;
+    int fim;
+}TFila;
 
-int fila[TAM];
-int C, IA = 0;
+TFila fila;
 
-void enqueue(int f[], int dado, int tamanho){
-    int i;
-    IA++;
-    if (IA == tamanho){
-        IA = 0;
-    }else{
-        for (i = 0; i < tamanho; i++){
-            
-            if (f[i] == 0){
-                f[i] = dado;
-                return;
-            } else {
-                f[IA] = dado;
-                return;
-            }
-        }
-    } 
-
-}
-
-
-void listar(int f[], int tamanho){
-    int i;
-    printf("\n\nListando a fila\n\n");
-    for (i = 0; i < tamanho;i++){
-        if (f[i] != 0)
-        {
-            printf("%d ",f[i]);
-        }
+void enqueue(int dado, int tamanho){
+    if((fila.fim != -1) && ((fila.fim+1)%tamanho) == fila.inicio){
+        printf("\n A fila está cheia");
+        return;
     }
+    fila.fim = (fila.fim + 1)%tamanho;
+    fila.fila[fila.fim] = dado;
 }
 
- int buscar( int f[], int tamanho, int chave){
-    int i;
-    for (i = 0; i < tamanho; i++){
-        if (f[i] == C){
+void dequeue(int tamanho){
+    fila.inicio = ((fila.inicio+1)%tamanho);
+}
+
+void listar(int tamanho){
+    int i = fila.inicio;
+    do{
+        printf("\n %d", fila.fila[i]);
+        i = ((i+1)%tamanho);
+    }while(i != (fila.fim + 1)%tamanho);
+}
+
+int buscar(int key, int tamanho){
+    int i = fila.inicio;
+    do{
+        if (fila.fila[i] == key){
             return i;
+        }
+        i = ((i+1)%tamanho);
+    }while(i != (fila.fim + 1)%tamanho);
+    return -1;
+}
+
+void main(int argc, char *agrv){
+    
+    int leitura, chave;
+    fila.inicio = 0;
+    fila.fim = -1;
+    
+    while(1){
+
+        printf("\n Insira um número na FILA: ");
+        scanf("%d", &leitura);
+        if(leitura == 0){
             break;
         }
+        enqueue(leitura, TAM);
     }
-    printf("\nValor não encontrado\n");
-    return -1;
-
- }
-
-int main(int argc, char** argv) {
-    int leitura;
-
-    while (1){
-        printf("indice atual : %d\n", IA);
-        printf("Informe o valor: ");
-        scanf("%d",&leitura);
-        if (leitura == 0)
-            break;
-        enqueue(fila,leitura,TAM);
+    
+    listar(TAM);
+    //dequeue(TAM);
+    //dequeue(TAM);
+    printf("\n Qual chave desejas buscar? ");
+    scanf("%d", &chave);
+    if (buscar(chave, TAM) == -1){
+        printf("\nChave não econtrada!");
+    } else { 
+        printf("\nChave encontrada no índice %d", buscar(chave, TAM));
     }
-
-    listar(fila,TAM);
-
-    printf("\n\nQual chave deseja buscar? ");
-    scanf("%d",&C);
-
-    int VC = buscar(fila,TAM,C);
-    if (VC != -1){
-        printf("\nChave encontrada no índice %d\n", VC);
-    } else {
-        printf("\nChave não encontrada\n");
-    }
-
-    return (EXIT_SUCCESS);
 }
